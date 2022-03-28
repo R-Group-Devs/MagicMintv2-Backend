0493c96485732b17b665ebaffdbec330e41f567f
StefanVelkoski <velkoskis@gmail.com>
2022-03-28 15:11:09 +0200

    merge

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.use(
       resave: true,
       proxy: true,
       saveUninitialized: true,
-      // cookie: {sameSite: 'none', secure: true, proxy: false, maxAge: 1000 * 60 * 60 * 24 * 7 },
+      cookie: {sameSite: 'none', secure: true, proxy: false, maxAge: 1000 * 60 * 60 * 24 * 7 },
 
   }));
   
